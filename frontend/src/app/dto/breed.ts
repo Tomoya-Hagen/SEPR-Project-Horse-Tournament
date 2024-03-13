773a295337a02a2cccf9ab6542b326bdd2b65868
Tomoya Vinzenz Kys Hagen <e12026650@student.tuwien.ac.at>
2024-03-13 18:51:24 +0100

    reset repository

--- frontend/src/app/dto/breed.ts
+++
@@ -1,4 +0,0 @@
-export interface Breed {
-  id: number;
-  name: string;
-}
--- frontend/src/app/util/date-helper.ts
+++
@@ -1,5 +0,0 @@
-import {formatDate} from "@angular/common";
-
-export function formatIsoDate(date: Date): string {
-  return formatDate(date, 'YYYY-MM-dd', 'en-DK');
-}
