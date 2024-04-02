8aabdc2eaef80988dc20bed979f25d6e3b8e49d9
Tomoya Vinzenz Kys Hagen <e12026650@student.tuwien.ac.at>
2024-04-02 23:03:57 +0200

    [US7] fix check style issues with indentations (backend)

--- backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/dto/TournamentStandingsTreeDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/dto/TournamentStandingsTreeDto.java
@@ -3,7 +3,7 @@ package at.ac.tuwien.sepr.assignment.individual.dto;
 import java.util.List;
 
 public record TournamentStandingsTreeDto(
-  TournamentDetailParticipantDto thisParticipant,
-  List<TournamentStandingsTreeDto> branches
-  ) {
-    }
+    TournamentDetailParticipantDto thisParticipant,
+    List<TournamentStandingsTreeDto> branches
+) {
+}
