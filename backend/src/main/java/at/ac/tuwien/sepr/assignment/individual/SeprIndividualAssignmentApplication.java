239d021e37d599d2debd0278e4c4918d35a026ee
Tomoya Vinzenz Kys Hagen <e12026650@student.tuwien.ac.at>
2024-04-10 00:18:40 +0200

    [TS10] add JavaDoc to SeprIndividualAssignmentApplication.java (backend)

--- backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/SeprIndividualAssignmentApplication.java
+++ backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/SeprIndividualAssignmentApplication.java
@@ -3,6 +3,9 @@ package at.ac.tuwien.sepr.assignment.individual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class to start the individual assignment application.
+ */
 @SpringBootApplication
 public class SeprIndividualAssignmentApplication {
 
